d8b783653ffff0a6da8b399bfa16e373938cf3a2
Reksa Milenia <reksa.milenia@gmail.com>
2020-05-12 21:40:23 +0700

    Updates

---
+++ precache-manifest.e495eeb488b5d962074077882e1ddbf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da7938192bc7e9890e70a1219e2b9b21",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "30d169296a39059deb49",
+    "url": "/movie/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "2ee65e9cf9eadbc36b27",
+    "url": "/movie/static/css/main.c499681e.chunk.css"
+  },
+  {
+    "revision": "30d169296a39059deb49",
+    "url": "/movie/static/js/2.68b29806.chunk.js"
+  },
+  {
+    "revision": "a45b3d84a52b91aa1760759ade5a8468",
+    "url": "/movie/static/js/2.68b29806.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ee65e9cf9eadbc36b27",
+    "url": "/movie/static/js/main.a5400483.chunk.js"
+  },
+  {
+    "revision": "f6490590c0cc94aa64b4",
+    "url": "/movie/static/js/runtime-main.420a4dfe.js"
+  }
+]);
